0f2864eaa5c526a066abd163a54cb4d970de346b
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-02-24 14:34:29 +0200

    feat: adaugă salut personalizat bazat pe ora zilei

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,3 +1,19 @@
+const dataCurenta = new Date();
+const ora = dataCurenta.getHours();
+const elementSalut = document.querySelector('header p');
+let mesaj = "";
+
+if(ora >=6 && ora < 12){
+    mesaj = "Buna dimineata! Bine ai venit pe pagina mea.";
+}else if(ora >= 12 && ora < 18){
+    mesaj = "Buna ziua! Bine ati venit pe pagina mea."
+}else{
+    mesaj = "Buna seara! Bine ai venit pe pagina mea.";
+}
+if (elementSalut) {
+    elementSalut.textContent = mesaj;
+}
+
 function submitForm() {
     const nume = document.getElementById("name").value;
     const email = document.getElementById("email").value;
