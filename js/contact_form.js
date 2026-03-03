a2d53057e5a830b7c5b3790ae621e0559ca85044
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-03-03 21:46:23 +0200

    feat: adauga toggle dark/light mode

--- js/contact_form.js
+++ js/contact_form.js
@@ -42,3 +42,16 @@ form.addEventListener('submit', function(event){
         feedbackElement.style.color = "green"; 
     }
 });
+
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', function(){
+
+    document.body.classList.toggle('dark-mode');
+
+    if(document.body.classList.contains('dark-mode')){
+        themeToggle.textContent = 'Light Mode';
+    }else{
+        themeToggle.textContent = 'Dark Mode';
+    }
+});
