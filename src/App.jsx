517f323f8dd7a17a32afbd3fa4c37ee3c2694869
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-03-18 12:43:06 +0200

    feat: adauga prima componenta App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+ return (
+ <div>
+ <h1>Dashboard</h1>
+ <p>Comanelea Raul Mihai </p>
+
+ <h2>Ce învăț astăzi:</h2>
+            <ul>
+                <li>Cum să șterg codul inutil</li>
+                <li>Cum să creez o componentă React</li>
+            </ul>
+    
+ </div>
+ );
+}
+export default App;
