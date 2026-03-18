4760792d4cf53d8f457573335e3b185522a57559
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-03-18 13:04:30 +0200

    feat: generare dinamica de carduri folosind map

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@
 import Card from './Card'; 
 
 function App() {
+    const projects = [
+        { title: "Proiect 1", description: "Pagina personala" },
+        { title: "Proiect 2", description: "Calculator buget" },
+        { title: "Proiect 3", description: "Dashboard React" },
+        
+        { title: "Proiect 4", description: "Joc X și 0" },
+        { title: "Proiect 5", description: "Aplicatie de Vreme" }
+    ];
     return (
         <div>
             <h1>Dashboard</h1>
-            <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
-            <Card title="Proiect 2" description="Pagina interactiva cu JavaScript" />
-            <Card title="Proiect 3" description="Dashboard cu React" />    
+           {projects.map(function(item, index) {
+                return <Card key={index} title={item.title} description={item.description} />;
+            })}   
         </div>
     );
 }
