6552956530a07a36d91be8f5bc9a207f034399eb
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-03-18 12:57:56 +0200

    feat: adauga componenta Card separata

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-function App() {
- return (
- <div>
- <h1>Dashboard</h1>
- <p>Comanelea Raul Mihai </p>
 
- <h2>Ce învăț astăzi:</h2>
-            <ul>
-                <li>Cum să șterg codul inutil</li>
-                <li>Cum să creez o componentă React</li>
-            </ul>
-    
- </div>
- );
+import Card from './Card'; 
+
+function App() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Card />    
+        </div>
+    );
 }
+
 export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+ return (
+ <div>
+    <h2>Acesta este un Card</h2>
+    <p>Aici se află o scurtă descriere a cardului scrisa hardcodat</p>
+ </div>
+ );
+}
+export default Card;
