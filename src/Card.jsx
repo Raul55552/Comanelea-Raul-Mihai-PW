f48514f74d64fc4ade97d1290728e7ddf3b85e83
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-03-18 13:00:24 +0200

    feat: folosire props pentru date dinamice in Card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
-function Card() {
- return (
- <div>
-    <h2>Acesta este un Card</h2>
-    <p>Aici se află o scurtă descriere a cardului scrisa hardcodat</p>
- </div>
- );
+function Card(props) {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </div>
+    );
 }
+
 export default Card;
