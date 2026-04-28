b7302f9adef1877d5d2a075db96b14e51289f608
Comanelea Raul Mihai <raul.comanelea@student.unitbv.ro>
2026-04-28 17:03:49 +0300

    feat: setup react router si creare pagini

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,18 @@
-
- import { useState } from 'react';
-import Card from './Card'; 
-import QuickNote from './QuickNote';
-import ToDoList from './ToDoList';  
-import ContactForm from './ContactForm';
-import ProjectList from './ProjectList';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
-    const projects = [
-        { title: "Proiect 1", description: "Pagina personala" },
-        { title: "Proiect 2", description: "Calculator buget" },
-        { title: "Proiect 3", description: "Dashboard React" },
-        
-        { title: "Proiect 4", description: "Joc X și 0" },
-        { title: "Proiect 5", description: "Aplicatie de Vreme" },
-        {title: "Proiect 6", description: "Joc nu te supara frate"}
-    ];
-    const [count, setCount] = useState(0);
-
-    function updateDownCount() {
-        if (count == 0)
-            return;
-        setCount(count - 1);
-    }
-
-    return (
-        <div>
-            <h1>Dashboard</h1>
-
-            <QuickNote />
-            <ToDoList />
-            <ContactForm />
-            <ProjectList />
-
-            <p>Ai apasat de {count} ori</p>
-            <button onClick={() => setCount(count + 1)}>+1</button>
-            <button onClick={updateDownCount}>-1</button>
-            <button onClick={() => setCount(0)}>Reset</button>
-
-           {projects.map(function(item, index) {
-                return <Card key={index} title={item.title} description={item.description} />;
-            })}   
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
